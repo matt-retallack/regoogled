c51aa07410c7733f487a1c8dfdaead63b1e52d68
Matthew Retallack <matthew.retallack@landmark.co.uk>
2019-04-04 09:58:50 +0100

    more tweaks to shell script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo 'Your Inject Account ID:'
-echo '(this won\'t be used for anything except to configure this app):'
+echo \(this won\'t be used for anything except to configure this app\):
 read account
 echo 'Configuring your account details...'
 sed -i "s/ACCOUNT/${account}/g" app.js
